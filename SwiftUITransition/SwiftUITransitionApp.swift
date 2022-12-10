56ee78dbf8655adb4e2894b6d94aa8a445c6205d
Rizal <rizal.es@hotmail.com>
2022-12-10 10:44:34 +0700

    Initial Commit

---
+++ SwiftUITransition/SwiftUITransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITransitionApp.swift
+//  SwiftUITransition
+//
+//  Created by Rizal on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
